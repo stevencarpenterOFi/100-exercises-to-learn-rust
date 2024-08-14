bb1113f38f0ff97745a87f9bf700300c9ea568f9
Steven Carpenter <steven.carpenter@orbitfab.com>
2024-08-14 11:08:58 -0600

    first 9 exercises done!

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,10 +1,19 @@
 // Rewrite the factorial function using a `while` loop.
-pub fn factorial(n: u32) -> u32 {
+pub fn factorial(mut n: u32) -> u32 {
     // The `todo!()` macro is a placeholder that the compiler
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result: u32 = 1;
+
+    while n >= 2 {
+
+        result = result * (n * (n-1));
+
+        n = n - 2;
+    }
+
+    result
 }
 
 #[cfg(test)]
